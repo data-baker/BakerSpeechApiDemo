837e1d064791e26d137a75f202b551980e5d74f6
wanghao <whforwork@163.com>
2021-06-08 10:44:08 +0800

    在线合成和录音文件识别

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # BakerSpeechApiDemo
-标贝各种api接入示例
-测试
+标贝科技官网：www.data-baker.com
+
+开发文档：https://www.data-baker.com/#/specs/file/product_intro
+
+## 简介
+
+本项目用于介绍标贝科技API接口如何调用。
---
+++ asr-rest-api/audio_file_recognition/Python/audio_file_recognition.py
@@ -0,0 +1,83 @@
+ #!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import time
+
+import requests
+import json
+
+
+# 获取access_token用于鉴权
+def get_access_token(client_secret, client_id):
+    grant_type = "client_credentials"
+    url = "https://openapi.data-baker.com/oauth/2.0/token?grant_type={}&client_secret={}&client_id={}".format(
+        grant_type, client_secret, client_id)
+    response = requests.post(url)
+    access_token = json.loads(response.text).get('access_token')
+
+    return access_token
+
+
+# 查询识别结果
+def get_text(headers):
+    url = "https://asr.data-baker.com/asr/taskresult"
+    response = requests.post(url, headers=headers)
+    text = json.loads(response.text).get("text")
+    code = json.loads(response.text).get("code")
+    if code != 20000:
+        raise Exception(response.text)
+
+    return text
+
+
+# 创建识别任务，发送识别文件
+def create_task(file, headers):
+    url = "https://asr.data-baker.com/asr/starttask?"
+    response = requests.post(url, data=file, headers=headers)
+    task_id = json.loads(response.text).get("taskid")
+    code = json.loads(response.text).get("code")
+    if code != 20000:
+        raise Exception(response.text)
+
+    return task_id
+
+
+# 获取命令行输入参数
+def get_args():
+    parser = argparse.ArgumentParser(description='ASR')
+    parser.add_argument('-client_secret', type=str, required=True)
+    parser.add_argument('-client_id', type=str, required=True)
+    parser.add_argument('-file_path', type=str, required=True)
+    parser.add_argument('--audio_format', type=str, default='wav')
+    parser.add_argument('--sample_rate', type=str, default='16000')
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    try:
+        args = get_args()
+
+        # 获取access_token
+        client_secret = args.client_secret
+        client_id = args.client_id
+        access_token = get_access_token(client_secret, client_id)
+
+        # 新建任务
+        file = open(args.file_path, 'rb')
+        audio_format = args.audio_format
+        sample_rate = args.sample_rate
+        headers = {'access_token': access_token, 'audio_format': audio_format, 'sample_rate': sample_rate}
+        task_id = create_task(file, headers)
+
+        # 根据录音文件大小，等待识别结束
+        time.sleep(1)
+
+        # 查询识别结果
+        recognition_headers = {'access_token': access_token, 'taskid': task_id}
+        text = get_text(recognition_headers)
+        print(text['left_result'])
+    except Exception as e:
+        print(e)
